aaefe56c4de4a2049d1415ce327bac5622ea6bcc
Nicolas Vigouroux <nvigouroux@eig.fr>
2023-01-03 15:35:20 +0100

    Mealcontroller

--- NuVirtualApi/Controllers/MealController.cs
+++ NuVirtualApi/Controllers/MealController.cs
@@ -1,6 +1,47 @@
-﻿namespace NuVirtualApi.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using NuVirtualApi.Domain.Interfaces.Business;
+using NuVirtualApi.Domain.Models.Request.Meal;
+using NuVirtualApi.Domain.Models.Request.NutritionGoal;
+using NuVirtualApi.Domain.Models.Response.Meal;
+
+[ApiController]
+[Route("[controller]")]
+public class MealController : Controller
 {
-    public class MealController
+    private static IMealBusiness _mealBusiness;
+
+    public MealController(IMealBusiness mealBusiness)
     {
+        _mealBusiness = mealBusiness;
+    }
+
+    [HttpPost]
+    public ActionResult<bool> CreateMeal([FromBody] CreateMealRequest request)
+    {
+        return _mealBusiness.CreateMeal(request);
+    }
+
+    [HttpDelete("id")]
+    public ActionResult<bool> DeleteMeal([FromRoute] int mealId)
+    {
+        return _mealBusiness.DeleteMeal(mealId);
+    }
+
+    [HttpGet]
+    public ActionResult<List<MealViewModel>> GetAllMealsByUserIdAndDate([FromHeader] int userId, [FromHeader] DateTime date)
+    {
+        var request = new GetAllMealsByUserIdAndDateRequest()
+        {
+            Date = date,
+            UserId = userId
+        };
+
+        return _mealBusiness.GetAllMealsByUserIdAndDate(request);
+    }
+
+    [HttpPut]
+    public ActionResult<bool> UpdateMeal([FromBody] UpdateMealRequest request)
+    {
+        return _mealBusiness.UpdateMeal(request);
     }
 }
